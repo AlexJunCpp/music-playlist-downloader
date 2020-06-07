c87f6444690f53f1f3b6ac152cbd2a4db9d283ef
zcmimi <zcmimi@outlook.com>
2020-06-07 22:08:36 +0800

    .

--- bundle.js
+++ bundle.js
@@ -4,18 +4,6 @@ var is_downloading=0,
     download_lrc=1,
     res="";
 $('#download_lrc').click(function(){download_lrc^=1;});
-function node(i){
-    return "<li class='mdui-list-item' style='max-width: 500px;'>\
-        <div class='mdui-list-item-avatar'><img src='"+list[i].pic+"'></div>\
-        <a onclick=\"play("+i+")\" class='mdui-list-item-content'>\
-            <div class='mdui-list-item-title'>"+list[i].title+"</div>\
-            <div class='mdui-list-item-text'>"+list[i].author+"</div>\
-        </a>\
-        <a href=\"javascript:download("+i+",1)\">\
-            \<i class='mdui-list-item-icon mdui-icon material-icons'>file_download</i>\
-        </a>\
-    </li>";
-}
 function play(i){
     now=i;
     $("#player").attr("src",list[i].url);
@@ -30,99 +18,118 @@ function nxt(){
 }
 function rnd(){play(Math.floor((Math.random()*(list.length-1))));}
 document.getElementById("player").addEventListener('ended',function(){nxt();});
+
+function get(){
+    var id=$('#playlistid').val(),
+        typ=$('#playlisttyp').val();
+    list=JSON.parse(
+        $.ajax({
+            url: "https://api.i-meto.com/meting/api?server="+typ+"&type=playlist&id="+id,
+            async: false
+        }).responseText
+    );
+}
+
 function sleep(ms){return new Promise(resolve=>setTimeout(resolve,ms));}
-function download(id,typ){
+function download_(id){
     var name=list[id].author+"-"+list[id].title;
     if($("#download_name").val()==1)name=list[id].title+"-"+list[id].author;
     else if($("#download_name").val()==2)name=list[id].title;
 
-    if(typ>0){
-        name=name.replace('/\/|\\|\||*|>|<|?|:/g','_');
-        var tmp=name,r="/\\*?<>|?:";name='';
-        for(var i=0;i<tmp.length;++i)
-            if(r.indexOf(tmp[i])!=-1)name+='_';
-            else name+=tmp[i];
-    }
+    var tmp=name,r="/\\*?<>|?:";name='';
+    for(var i=0;i<tmp.length;++i)
+        if(r.indexOf(tmp[i])!=-1)name+='_';
+        else name+=tmp[i];
 
-    if(typ==2)
-        res+="wget \""+list[i].url+"\" -O \""+name+".mp3\"\n";
-    else if(typ==3)
-        res+="certutil -urlcache -split -f \""+list[i].url+"\" \""+name+".mp3\"\n";
-    else{
-        var xhr=new XMLHttpRequest();
-        xhr.open('GET',list[id].url,true);
-        xhr.responseType="blob";
-        xhr.onload=async function(){
-            is_downloading=1;
-            if(this.status===200){
-                var blob=this.response;
-                var a=document.createElement('a');
-                a.download=name+".mp3";
-                a.href=window.URL.createObjectURL(blob);
-                a.click();
-                await sleep(2000);
-                is_downloading=0;
-            }
-        };
-        xhr.send();
-    }
+    var xhr=new XMLHttpRequest();
+    xhr.open('GET',list[id].url,true);
+    xhr.responseType="blob";
+    xhr.onload=async function(){
+        is_downloading=1;
+        if(this.status===200){
+            var blob=this.response;
+            var a=document.createElement('a');
+            a.download=name+".mp3";
+            a.href=window.URL.createObjectURL(blob);
+            a.click();
+            await sleep(2000);
+            is_downloading=0;
+        }
+    };
+    xhr.send();
     if(!download_lrc)return;
-    if(typ==2)
-        res+="wget \""+list[i].lrc+"\" -O \""+name+".lrc\"\n";
-    else if(typ==3)
-        res+="certutil -urlcache -split -f \""+list[i].lrc+"\" \""+name+".lrc\"\n";
-    else{
-        var xhr=new XMLHttpRequest();
-        xhr.open('GET',list[id].lrc,true);
-        xhr.responseType="blob";
-        xhr.onload=async function(){
-            is_downloading=1;
-            if(this.status===200){
-                var blob=this.response;
-                var a=document.createElement('a');
-                a.download=name+".lrc";
-                a.href=window.URL.createObjectURL(blob);
-                a.click();
-                await sleep(1000);
-                is_downloading=0;
-            }
-        };
-        xhr.send();
+    var xhr=new XMLHttpRequest();
+    xhr.open('GET',list[id].lrc,true);
+    xhr.responseType="blob";
+    xhr.onload=async function(){
+        is_downloading=1;
+        if(this.status===200){
+            var blob=this.response;
+            var a=document.createElement('a');
+            a.download=name+".lrc";
+            a.href=window.URL.createObjectURL(blob);
+            a.click();
+            await sleep(1000);
+            is_downloading=0;
+        }
+    };
+    xhr.send();
+}
+async function download(){
+    mdui.snackbar({message: '加载中',timeout: 500,position: 'top'});
+    get();
+    mdui.snackbar({message: '开始下载',timeout: 500,position: 'top'});
+    for(i in list){
+        download_(i);
+        while(is_downloading)await sleep(500);
     }
 }
-async function render(b){
+function expt_(id,typ){
+    var name=list[id].author+"-"+list[id].title;
+    if($("#download_name").val()==1)name=list[id].title+"-"+list[id].author;
+    else if($("#download_name").val()==2)name=list[id].title;
+    
+    var tmp=name,r="/\\*?<>|?:";name='';
+    for(var i=0;i<tmp.length;++i)
+        if(r.indexOf(tmp[i])!=-1)name+='_';
+        else name+=tmp[i];
+
+    if(typ=='wget')
+        res+="wget \""+list[i].url+"\" -O \""+name+".mp3\"\n";
+    else if(typ=='certutil')
+        res+="certutil -urlcache -split -f \""+list[i].url+"\" \""+name+".mp3\"\n";
+    if(!download_lrc)return;
+    if(typ=='wget')
+        res+="wget \""+list[i].lrc+"\" -O \""+name+".lrc\"\n";
+    else if(typ=='certutil')
+        res+="certutil -urlcache -split -f \""+list[i].lrc+"\" \""+name+".lrc\"\n";
+}
+function expt(typ){
+    mdui.snackbar({message: '加载中',timeout: 500,position: 'top'});
+    get();
+    res="";
+    for(i in list)expt_(i,typ);
+    console.clear(),console.log(res);
+    mdui.snackbar({message: '加载完毕',timeout: 500,position: 'top'});
     mdui.snackbar({
-        message: '加载中',
-        timeout: 500,
-        position: 'top'
+        message:"请按F12打开控制台(Console)复制,并在终端运行",
+        timeout:5000,position:'top'
     });
-    if(b>1)res="";
-    if(b==0)$("#songlist").html("");
-    var id=$('#playlistid').val(),
-        typ=$('#playlisttyp').val();
-    var t=$.ajax({
-        url: "https://api.i-meto.com/meting/api?server="+typ+"&type=playlist&id="+id,
-        async: false
-    }).responseText;
-    list=JSON.parse(t);
-    for(i in list)
-        if(b==0)$("#songlist").append(node(i));
-        else{
-            download(i,b);
-            while(is_downloading)await sleep(500);
-        }
-    mdui.mutation();
-    mdui.snackbar({
-        message: '加载完毕',
-        timeout: 500,
-        position: 'top'
-    });
-    if(b>1){
-        mdui.snackbar({
-            message:"请按F12打开控制台(Console)复制,并在终端运行",
-            timeout:5000,
-            position:'top'
-        });
-        console.log(res);
-    }
+}
+function genlist(){
+    mdui.snackbar({message: '加载中',timeout: 500,position: 'top'});
+    $("#songlist").html("");
+    get();
+    for(i in list)$("#songlist").append(
+"<li class='mdui-list-item' style='max-width: 500px;'>\
+    <div class='mdui-list-item-avatar'><img src='"+list[i].pic+"'></div>\
+    <a onclick=\"play("+i+")\" class='mdui-list-item-content'>\
+        <div class='mdui-list-item-title'>"+list[i].title+"</div>\
+        <div class='mdui-list-item-text'>"+list[i].author+"</div>\
+    </a>\
+    <a href=\"javascript:download_("+i+")\">\
+        \<i class='mdui-list-item-icon mdui-icon material-icons'>file_download</i>\
+    </a>\
+</li>");
+    mdui.snackbar({message: '加载完毕',timeout: 500,position: 'top'});
 }
