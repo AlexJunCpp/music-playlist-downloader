524f515fc032842165b70e557f3f55bcb46bc32c
zcmimi <zcmimi@outlook.com>
2020-06-08 10:41:20 +0800

    .

--- bundle.js
+++ bundle.js
@@ -2,11 +2,72 @@ var is_downloading=0,
     list,
     now=0,
     download_lrc=1,
-    res="";
+    res="",
+    audio=$('#player')[0],
+    lrcTime=[],
+    lrc,
+    lrcli,
+    currentLine,
+    currentTime,
+    lrc_ppxx,
+    songlist,
+    screencenter=window.innerHeight/2;
 $('#download_lrc').click(function(){download_lrc^=1;});
+function getlrc(id){
+    var t=$.ajax({
+        url: list[id].lrc,
+        async: false
+    }).responseText;
+    lrcTime=[];
+    lrcul=$("#lrclist")[0];lrcul.innerHTML="";
+    lrcul.style.transform="translateY("+screencenter+"px)";
+    currentLine=0;
+    if(t==''){
+        lrcul.innerHTML='<center>暂无歌词<center>';
+        return;
+    }
+    t=t.split('\n');
+    for(var i=0;i<t.length;++i){
+        lrcTime[i]=parseFloat(t[i].substr(1,3))*60+parseFloat(t[i].substring(4,10));//00:00.000转化为00.000格式
+        lrcul.innerHTML+="<li class='mdui-list-item'>"+t[i].substr(11,t[i].length)+"</li>";
+    }
+	lrcTime[lrcTime.length]=lrcTime[lrcTime.length-1]+3;
+    lrcli=$("#lrclist>li");    
+}
+audio.ontimeupdate=function(){
+	currentTime=audio.currentTime;
+	for(var j=currentLine,len=lrcTime.length;j<len;j++){
+		if (currentTime<lrcTime[j+1] && currentTime>lrcTime[j]){
+			currentLine=j;
+			lrc_ppxx=screencenter-(currentLine*48);
+			lrcul.style.transform="translateY("+lrc_ppxx+"px)";
+			lrcli.get(currentLine-1).className="mdui-list-item";
+			lrcli.get(currentLine).className="mdui-list-item on";
+			break;
+		}
+	}
+};
+audio.onseeked=function(){
+    currentTime=audio.currentTime;
+    lrcli.get(currentLine).className="";
+    for(k=0,len=lrcTime.length;k<len;++k){
+        if(currentTime<lrcTime[k+1]&&currentTime<lrcTime[k]){
+            currentLine=k;
+            break;
+        }
+    }
+};
+audio.onerror=function(){
+    mdui.snackbar({message: '播放失败,自动下一首',timeout: 500,position: 'top'});
+    nxt();
+};
 function play(i){
+    songlist[now].children[1].style.fontWeight='normal';
     now=i;
     $("#player").attr("src",list[i].url);
+    getlrc(i);
+    songlist[i].scrollIntoView(false);
+    songlist[i].children[1].style.fontWeight='bold';
 }
 function pre(){
     if(now>0)play(now-1);
@@ -121,7 +182,7 @@ function genlist(){
     $("#songlist").html("");
     get();
     for(i in list)$("#songlist").append(
-"<li class='mdui-list-item' style='max-width: 500px;'>\
+"<li class='mdui-list-item'>\
     <div class='mdui-list-item-avatar'><img src='"+list[i].pic+"'></div>\
     <a onclick=\"play("+i+")\" class='mdui-list-item-content'>\
         <div class='mdui-list-item-title'>"+list[i].title+"</div>\
@@ -131,5 +192,6 @@ function genlist(){
         \<i class='mdui-list-item-icon mdui-icon material-icons'>file_download</i>\
     </a>\
 </li>");
+    songlist=lrcli=$("#songlist>li");
     mdui.snackbar({message: '加载完毕',timeout: 500,position: 'top'});
 }
